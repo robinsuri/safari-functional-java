6ff66dd142fe369b15bac7f57e8c3f3fc4e00f2e
SimonHGR <simon@dancingcloudservices.com>
2020-09-08 07:22:43 -0700

    Day 1 Break 2

--- src/main/java/students/School.java
+++ src/main/java/students/School.java
@@ -1,28 +1,38 @@
 package students;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-interface StudentTest {
-    boolean test(Student s);
+@FunctionalInterface
+interface Test<E> {
+    default void doDefaultStuff(){}
+    boolean test(E s);
+//    void doStuff();
 }
 
-class SmartStudentTest implements StudentTest {
+class SmartStudentTest implements Test<Student> {
     public boolean test(Student s) {
         return s.getGrade() > 70;
     }
 }
 
-class EnthusiasticStudentTest implements StudentTest {
+//class ShortStringTest implements Test<String> {
+//    @Override
+//    public boolean test(String s) {
+//        return s.length() < 5;
+//    }
+//}
 
+class EnthusiasticStudentTest implements Test<Student> {
     @Override
     public boolean test(Student s) {
         return s.getCourses().size() > 3;
     }
 }
 public class School {
-    public static void showStudents(List<Student> ls) {
-        for (Student s : ls) {
+    public static <E> void show(List<E> ls) {
+        for (E s : ls) {
             System.out.println(s);
         }
         System.out.println("-----------------");
@@ -31,10 +41,10 @@ public class School {
     // Use "pointer to an object" for the behavior that object contains...
     // e.g. strategy, and this one, which is called "command"
     // In "functional programming" passing a "behavior" (i.e. a function :) is called "higher order function"
-    public static List<Student> getStudents(List<Student> ls, StudentTest sst) {
-        List<Student> rv = new ArrayList<>();
-        for (Student s : ls) {
-            if (sst.test(s)) {
+    public static <E> List<E> filter(List<E> ls, Test<E> sst) {
+        List<E> rv = new ArrayList<>();
+        for (E s : ls) {
+            if (sst.test(s)) { // NOT sst(s)
                 rv.add(s);
             }
         }
@@ -68,8 +78,58 @@ public class School {
                 Student.of("Sheila", 92, "Math", "Physics", "Astrophysics", "Quantum Mechanics")
         );
 
-        showStudents(getStudents(roster, new SmartStudentTest()));
+        show(filter(roster, new SmartStudentTest()));
 //        showStudents(getSmartStudents(roster, 70));
-        showStudents(getStudents(roster, new EnthusiasticStudentTest()));
+        show(filter(roster, new EnthusiasticStudentTest()));
+
+        List<String> names = List.of("Fred", "Jim", "Sheila");
+//        show(filter(names, new ShortStringTest()));
+
+//        show(filter(names, new
+//                // "Anonymous inner class"
+//        /*class ShortStringTest implements */Test<String>() {
+//            @Override
+//            public boolean test(String s) {
+//                return s.length() < 5;
+//            }
+//        }
+//                ));
+
+        // "Traditional" compiler mode -- tell what you must give, then verify I gave the right thing...
+        // "New" mode, "here are parts of what's needed... work out what is fully needed, and make it from these parts..
+        // which parts are needed???
+        // compiler knows it needs an object--no value in saying "new"
+        // knows it must be a Test<String>
+        // In THIS situation, the *inteface* defines EXACTLY ONE abstract method, AND we only want to implement
+        // that ONE abstract method...
+        // don't need "grouping"
+        // Compiler knows the ONLY method we're implementing is ... test(String s)
+        // needs "permission" to use "new syntax" and behavior ... use an arrow
+//        show(filter(names, /*new Test<String>() { */
+//           /* @Override
+//            public boolean test*/(String s) -> {
+//                return s.length() < 5;
+//            }
+//        /*}*/
+//                ));
+//        show(filter(names, (String s) -> { return s.length() < 5; }));
+        // if argument types are ALL unambiguous, can leave them ALL out
+//        show(filter(names, (s) -> { return s.length() < 5; }));
+        // java 11 allows var for arg types (ALL or none)
+//        show(filter(names, (var s) -> { return s.length() < 5; }));
+//        show(filter(names, (@Deprecated var s) -> { return s.length() < 5; }));
+        // IFF exactly ONE argument, without type of any kind, can leave off parens
+        // zero args MUST have parens e.g. () -> {}
+//        show(filter(names, s -> { return s.length() < 5; }));
+        // IFF function body is purely a single return statement, replace the body with the expression to be returned
+        show(filter(names, s -> s.length() < 5 ));
+
+        Test<String> obj = s -> s.length() < 5;
+        System.out.println("Class of the lambda is " + obj.getClass().getName());
+        System.out.println("lambda implements Test? " + (obj instanceof Test));
+        Method[] methods = obj.getClass().getMethods();
+        for (Method m : methods) {
+            System.out.println(m);
+        }
     }
 }
